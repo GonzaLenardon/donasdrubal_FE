f1e19baf6bd3d0e23c6861d150bd12dd35dafa87
informatica22023 <adrianlencina@frp.utn.edu.ar>
2026-09-22 19:04:15 -0300

    mod-gen-pdfs

--- src/utils/generarPdf.js
+++ src/utils/generarPdf.js
@@ -1,18 +1,31 @@
 const generarPDF = (blob, filename, mode = 'preview') => {
+
+  console.log('PDF recibido en frontend:', {
+    type: blob?.type,
+    size: blob?.size,
+    filename,
+  });
+
   const url = URL.createObjectURL(blob);
 
-  console.log('Que llega ', mode);
+  // console.log('URL creada:', url);
 
   if (mode === 'download') {
+
     const a = document.createElement('a');
     a.href = url;
     a.download = filename;
+    document.body.appendChild(a);
     a.click();
+    a.remove();
+
   } else {
+
     window.open(url, '_blank');
   }
 
-  setTimeout(() => URL.revokeObjectURL(url), 10000);
+  // Por ahora NO revocar la URL
+  setTimeout(() => URL.revokeObjectURL(url), 60000);
 };
 
 export default generarPDF;
